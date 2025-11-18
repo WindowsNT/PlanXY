e927ef1d6bf7e6400cdd4a3779e2de1dd67a787a
Michael Chourdakis <chourdakismichael@gmail.com>
2025-11-18 15:41:55 +0200

    Update

--- msa2fet/msa2fet.cpp
+++ msa2fet/msa2fet.cpp
@@ -648,7 +648,7 @@ int msa2fetmain(const wchar_t* dbname,const wchar_t* targetfet,int stype)
 
 	auto& root = x.GetRootElement();
 	root.SetElementName("fet");
-	root.vv("version").SetValue("7.5.6");
+	root.vv("version").SetValue("7.5.7");
 	if (stype == 99)
 	{
 		LoadParametersMSA();
--- version.h
+++ version.h
@@ -1,1 +1,1 @@
-#define VERSION "1.206.0.0"
+#define VERSION "1.207.0.0"
